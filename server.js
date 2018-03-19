994a3e52e0233f02bd5ea189149ad379c40b9ddc
Fabian Montemayor <f.montemayor26@gmail.com>
2018-03-19 11:55:16 -0600

    typo

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ app.use('/tutors', tutors);
 app.use('/user', user);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Length");
-  res.header("Access-Control-Allow-Credentials",  "true");
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
+  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Length");
+  req.header("Access-Control-Allow-Credentials",  "true");
 
   if ('OPTIONS' === req.method){
     res.send(200);
