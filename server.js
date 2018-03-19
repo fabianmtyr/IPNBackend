a457d22c04545c7021d688df505acbfa3bb8d43a
Fabian Montemayor <f.montemayor26@gmail.com>
2018-03-19 12:23:48 -0600

    localhost aded

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/tutors', tutors);
 app.use('/user', user);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", "https://localhost:4200");
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
